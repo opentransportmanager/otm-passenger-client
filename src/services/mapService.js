0e5feed49f046a3122f49517f465c48291706a02
KaSliK <kamil.kaslikowski@gmail.com>
2020-07-14 17:33:45 +0200

    Added functions that getting path to draw and drawing route in mapService

--- src/services/mapService.js
+++ src/services/mapService.js
@@ -35,5 +35,47 @@ export default {
     return axios.get(`/paths/${pathId}/stations`).catch(() => {
       store.dispatch("showError");
     });
+  },
+
+  getPathToDraw(pathId) {
+    return axios.get(`paths/${pathId}/stations`).then(response => {
+      const stations = response.data;
+      const endStation = stations[stations.length - 1].position;
+      const startStation = stations[0].position;
+      const waypoints = [];
+      for (let i = 1; i < stations.length - 1; i++) {
+        waypoints[i - 1] = { location: stations[i].position, stopover: true };
+      }
+      return {
+        startStation: startStation,
+        endStation: endStation,
+        waypoints: waypoints
+      };
+    });
+  },
+
+  drawRoute(
+    directionsService,
+    directionsDisplay,
+    startStation,
+    endStation,
+    waypoints
+  ) {
+    directionsService.route(
+      {
+        origin: startStation,
+        destination: endStation,
+        waypoints: waypoints,
+        travelMode: "WALKING"
+      },
+      function(response, status) {
+        if (status === "OK") {
+          directionsDisplay.setOptions({ suppressMarkers: true });
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
   }
 };
